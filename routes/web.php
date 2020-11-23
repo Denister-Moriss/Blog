1238e84405073c1906c80cf242b4676d4420d0f5
Denister-Moriss <denister2002@gmail.com>
2020-11-23 14:44:24 +0200

    Add resource routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 
 Route::get('/', [PagesController::class,'index']);
 Route::get('/about', [PagesController::class,'about']);
 Route::get('/services', [PagesController::class,'services']);
+
+Route::resource('posts',PostsController::class);
